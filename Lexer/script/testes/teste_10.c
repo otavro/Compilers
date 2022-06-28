e1c437c6ac45392b4cc972bc801092ae0a5edc27
Otavio Reis <otavio.reis366@gmail.com>
2022-06-28 14:39:07 -0300

    Adding files

---
+++ Lexer/script/testes/teste_10.c
@@ -0,0 +1,87 @@
+foo (a)
+{
+  int b = a + 1;
+  int c = (short) a;
+  if (b)
+    return b;
+  return 1;
+}
+
+int funcao(int a, char b)
+{
+    int i;
+    char c;
+    
+    07 + 08 + 007 + 008 + 0x9 + 0X9;
+    
+    c = *funcao + "oi mundo"; 
+    
+    
+    return (0xa + 0XA + 007 + 07);
+}
+/*
+
+/* comentario // comentario
+
+// comentario 
+
+if(a>b)
+{
+    identificador = a > b;
+    i = 666;
+}
+
+/* comentario
+
+*/
+
+
+if(1)
+{
+    ff = (int) 23/ *i;
+    ch = '\a';
+    ch = '\b';
+    ch = '\f';
+    ch = '\n';
+    ch = '\r';
+    ch = '\t';
+    ch = '\v';
+    ch = '\\';
+    ch = '\'';
+    ch = '\"';    
+    ch = '\?';
+    ch = '\0';
+
+    ch = '!';
+    ch = '@';
+    ch = '#';
+    ch = '$';
+    ch = '%';
+    ch = '^';
+    ch = '&';
+    ch = '*';
+    ch = '(';
+    ch = ')';
+    ch = '_';
+    ch = '?';
+    ch = ' ';
+    ch = '+';
+    ch = '-';
+    ch = '/';
+    ch = '.';
+    ch = ':';
+    ch = ';';
+    ch = '<';
+    ch = '=';
+    ch = '>';
+    ch = ',';
+    ch = '[';
+    ch = ']';
+    ch = '}';
+    ch = '{';
+    ch = '`';
+    ch = '~';
+    ch = '|';
+/* oi */  /* isto eh um
+   comentario iniciado
+   e nao terminado
